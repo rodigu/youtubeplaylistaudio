5bcdc0ba3a40c78458666b105d36022d8c42438b
Rodrigo <rmoraisu@protonmail.com>
2020-09-19 09:07:43 -0300

    :file_folder: mkdir command fix

--- youtubedl.py
+++ youtubedl.py
@@ -4,8 +4,8 @@ import pathlib
 link = input("Playlist link: ")
 write = "youtube-dl " + link + " -i -f 140"
 filename = input("Name for the file: ")
-create = "mkdir " + filename
-os.system(create)
+safe_name = filename.replace('/', '_')
+os.mkdir(os.path.join(file_directory,safe_name))
 PATH = str(pathlib.Path(__file__).parent.absolute()) + '/' + filename
 download = "youtube-dl -i -f 140 -o " + PATH + "/%\(title\)s.%\(ext\)s " + link
 os.system(download)
