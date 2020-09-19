71b9b5827a7fe24a9d420ef5f967bffdd64c0a8b
Rodrigo <rmoraisu@protonmail.com>
2020-09-19 09:22:46 -0300

    Variable names

--- youtubedl.py
+++ youtubedl.py
@@ -5,7 +5,7 @@ link = input("Playlist link: ")
 write = "youtube-dl " + link + " -i -f 140"
 filename = input("Name for the file: ")
 safe_name = filename.replace('/', '_')
-os.mkdir(os.path.join(file_directory,safe_name))
+os.mkdir(safe_name)
 PATH = str(pathlib.Path(__file__).parent.absolute()) + '/' + filename
 download = "youtube-dl -i -f 140 -o " + PATH + "/%\(title\)s.%\(ext\)s " + link
 os.system(download)
